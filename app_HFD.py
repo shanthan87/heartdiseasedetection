886e283200526612277a83a09734a4c05ae6340a
shanthan87 <57036225+shanthan87@users.noreply.github.com>
2021-06-06 00:58:34 +0100

    Update app_HFD.py

--- app_HFD.py
+++ app_HFD.py
@@ -1,9 +1,12 @@
 import pandas as pd
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import accuracy_score
-from sklearn.model_selection import train_test_split
 from PIL import Image
 import streamlit as st
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import confusion_matrix, accuracy_score
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 
@@ -30,11 +33,71 @@ with col3:
 
 df = pd.read_csv('DataSet.csv')
 
-X = df.iloc[:, 0:12].values
-Y = df.iloc[:, -1].values
 
-X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=0)
+x = df.iloc[:, 0:12].values
+y = df.iloc[:, -1].values
 
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=0)
+
+sc = StandardScaler()
+x_train = sc.fit_transform(x_train)
+x_test = sc.transform(x_test)
+
+# ********************
+# logistic Regression
+# ********************
+classifier = LogisticRegression()
+classifier.fit(x_train, y_train)
+
+y_predlr = classifier.predict(x_test)
+
+mylist = []
+cm = confusion_matrix(y_test, y_predlr)
+ac = accuracy_score(y_test, y_predlr)
+mylist.append(ac)
+
+# ****************
+# Decision Tree
+# ******************
+list1 = []
+for leaves in range(2, 10):
+    classifier = DecisionTreeClassifier(max_leaf_nodes=leaves, random_state=0, criterion='entropy')
+    classifier.fit(x_train, y_train)
+    y_pred = classifier.predict(x_test)
+    list1.append(accuracy_score(y_test, y_pred))
+
+classifier = DecisionTreeClassifier(max_leaf_nodes=3, random_state=0, criterion='entropy')
+classifier.fit(x_train, y_train)
+
+y_preddt = classifier.predict(x_test)
+
+cm = confusion_matrix(y_test, y_preddt)
+ac = accuracy_score(y_test, y_preddt)
+mylist.append(ac)
+
+# *************************************
+# Random Forest
+# **************************************
+list1 = []
+for estimators in range(10, 30):
+    classifier = RandomForestClassifier(n_estimators=estimators, random_state=0, criterion='entropy')
+    classifier.fit(x_train, y_train)
+    y_pred = classifier.predict(x_test)
+    list1.append(accuracy_score(y_test, y_pred))
+
+classifier = RandomForestClassifier(n_estimators=11, criterion='entropy', random_state=0)
+classifier.fit(x_train, y_train)
+
+y_predrf = classifier.predict(x_test)
+
+cm = confusion_matrix(y_test, y_predrf)
+ac = accuracy_score(y_test, y_predrf)
+mylist.append(ac)
+
+
+# **********************
+# Side Bar or Slider
+# **************************
 
 def get_user_input():
     age = st.sidebar.text_input("Age", 0)
@@ -70,6 +133,34 @@ def get_user_input():
 
 user_input = get_user_input()
 
+st.title('Chances of Heart Diseases')
+
+col1, col2, col3 = st.beta_columns([1, 1, 1])
+
+with col1:
+    LogisticRegression = LogisticRegression()
+    LogisticRegression.fit(x_train, y_train)
+
+    st.write('Logistic Regression')
+    st.write(str(accuracy_score(y_test, LogisticRegression.predict(x_test)) * 100) + '%')
+    y_pred = LogisticRegression.predict(user_input)
+
+with col2:
+    DecisionTreeClassifier = DecisionTreeClassifier()
+    DecisionTreeClassifier.fit(x_train, y_train)
+
+    st.write('Decision Tree Classifier')
+    y_preddt = DecisionTreeClassifier.predict(user_input)
+    st.write(str(accuracy_score(y_test, DecisionTreeClassifier.predict(x_test)) * 100) + '%')
+
+with col3:
+    RandomForestClassifier = RandomForestClassifier()
+    RandomForestClassifier.fit(x_train, y_train)
+
+    st.write('Random Forest')
+    y_predrf = RandomForestClassifier.predict(user_input)
+    st.write(str(accuracy_score(y_test, RandomForestClassifier.predict(x_test)) * 100) + '%')
+
 st.subheader('Current Data Input')
 st.write(user_input)
 
@@ -80,19 +171,11 @@ with col1:
     st.write('Diabetes (0 = No , 1 = Yes)')
 
 with col2:
-    st.write('HBP (0 = No , 1 = Yes)')
-
-with col3:
-    st.write('Smoke (0 = No , 1 = Yes)')
     st.write('Gender (0 = F , 1 = M)')
 
-RandomForestClassifier = RandomForestClassifier()
-RandomForestClassifier.fit(X_train, Y_train)
-
-st.title('Chances of Heart Diseases')
-st.write(str(accuracy_score(Y_test, RandomForestClassifier.predict(X_test)) * 100) + '%')
-
-prediction = RandomForestClassifier.predict(user_input)
+with col3:
+    st.write('HBP (0 = No , 1 = Yes)')
+    st.write('Smoke (0 = No , 1 = Yes)')
 
 st.title('Dataset')
 
@@ -108,20 +191,20 @@ values2 = [len(d1), len(d2), len(d3), len(d4)]
 
 # Create subplots: use 'domain' type for Pie subplot
 fig = make_subplots(rows=1, cols=2, specs=[[{'type': 'domain'}, {'type': 'domain'}]])
-fig.add_trace(go.Pie(labels=label1, values=values1, name="GENDER"),
+fig.add_trace(go.Pie(labels=label1, values=values1, name=""),
               1, 1)
-fig.add_trace(go.Pie(labels=label2, values=values2, name="GENDER VS DEATH"),
+fig.add_trace(go.Pie(labels=label2, values=values2, name=""),
               1, 2)
 
 # Use `hole` to create a donut-like pie chart
 fig.update_traces(hole=.4, hoverinfo="label+percent")
 
 fig.update_layout(
-    title_text="GENDER  \
-                   GENDER VS DEATH",
+    title_text="SEX  \
+                   SEX VS DEATH",
     # Add annotations in the center of the donut pies.
-    annotations=[dict(text='GENDER', x=0.19, y=0.5, font_size=10, showarrow=False),
-                 dict(text='GENDER VS DEATH', x=0.84, y=0.5, font_size=9, showarrow=False)],
+    annotations=[dict(text='SEX', x=0.19, y=0.5, font_size=10, showarrow=False),
+                 dict(text='SEX VS DEATH', x=0.84, y=0.5, font_size=9, showarrow=False)],
     autosize=False, width=700, height=500, paper_bgcolor="white")
 
 st.plotly_chart(fig)
