3ac2d877d7addb123afcd9369c9ba92b69aa8777
shanthan87 <57036225+shanthan87@users.noreply.github.com>
2021-06-08 12:26:50 +0100

    Update app_HFD.py

--- app_HFD.py
+++ app_HFD.py
@@ -5,7 +5,6 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import confusion_matrix, accuracy_score
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
@@ -19,20 +18,14 @@ col1, col2, col3 = st.beta_columns([1, 1, 1])
 
 with col1:
     st.title('Heart')
-    st.write('Random')
 
 with col2:
     st.title('Failure')
-    st.write('Forest')
-    image = Image.open('heart.png')
-    st.image(image, use_column_width=True)
 
 with col3:
     st.title('Detection')
-    st.write('Algorithm')
 
 df = pd.read_csv('DataSet.csv')
-
 
 x = df.iloc[:, 0:12].values
 y = df.iloc[:, -1].values
@@ -42,19 +35,6 @@ x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_
 sc = StandardScaler()
 x_train = sc.fit_transform(x_train)
 x_test = sc.transform(x_test)
-
-# ********************
-# logistic Regression
-# ********************
-classifier = LogisticRegression()
-classifier.fit(x_train, y_train)
-
-y_predlr = classifier.predict(x_test)
-
-mylist = []
-cm = confusion_matrix(y_test, y_predlr)
-ac = accuracy_score(y_test, y_predlr)
-mylist.append(ac)
 
 # ****************
 # Decision Tree
@@ -71,9 +51,6 @@ classifier.fit(x_train, y_train)
 
 y_preddt = classifier.predict(x_test)
 
-cm = confusion_matrix(y_test, y_preddt)
-ac = accuracy_score(y_test, y_preddt)
-mylist.append(ac)
 
 # *************************************
 # Random Forest
@@ -89,10 +66,6 @@ classifier = RandomForestClassifier(n_estimators=11, criterion='entropy', random
 classifier.fit(x_train, y_train)
 
 y_predrf = classifier.predict(x_test)
-
-cm = confusion_matrix(y_test, y_predrf)
-ac = accuracy_score(y_test, y_predrf)
-mylist.append(ac)
 
 
 # **********************
@@ -138,20 +111,15 @@ st.title('Chances of Heart Diseases')
 col1, col2, col3 = st.beta_columns([1, 1, 1])
 
 with col1:
-    LogisticRegression = LogisticRegression()
-    LogisticRegression.fit(x_train, y_train)
-
-    st.write('Logistic Regression')
-    st.write(str(accuracy_score(y_test, LogisticRegression.predict(x_test)) * 100) + '%')
-    y_pred = LogisticRegression.predict(user_input)
-
-with col2:
     DecisionTreeClassifier = DecisionTreeClassifier()
     DecisionTreeClassifier.fit(x_train, y_train)
 
     st.write('Decision Tree')
     y_preddt = DecisionTreeClassifier.predict(user_input)
     st.write(str(accuracy_score(y_test, DecisionTreeClassifier.predict(x_test)) * 100) + '%')
+
+with col2:
+    st.write('-')
 
 with col3:
     RandomForestClassifier = RandomForestClassifier()
@@ -176,35 +144,3 @@ with col2:
 with col3:
     st.write('HBP (0 = No , 1 = Yes)')
     st.write('Smoke (0 = No , 1 = Yes)')
-
-st.title('Dataset')
-
-d1 = df[(df["DEATH_EVENT"] == 0) & (df["sex"] == 1)]
-d2 = df[(df["DEATH_EVENT"] == 1) & (df["sex"] == 1)]
-d3 = df[(df["DEATH_EVENT"] == 0) & (df["sex"] == 0)]
-d4 = df[(df["DEATH_EVENT"] == 1) & (df["sex"] == 0)]
-
-label1 = ["Male", "Female"]
-label2 = ['Male - Survived', 'Male - Died', "Female -  Survived", "Female - Died"]
-values1 = [(len(d1) + len(d2)), (len(d3) + len(d4))]
-values2 = [len(d1), len(d2), len(d3), len(d4)]
-
-# Create subplots: use 'domain' type for Pie subplot
-fig = make_subplots(rows=1, cols=2, specs=[[{'type': 'domain'}, {'type': 'domain'}]])
-fig.add_trace(go.Pie(labels=label1, values=values1, name=""),
-              1, 1)
-fig.add_trace(go.Pie(labels=label2, values=values2, name=""),
-              1, 2)
-
-# Use `hole` to create a donut-like pie chart
-fig.update_traces(hole=.4, hoverinfo="label+percent")
-
-fig.update_layout(
-    title_text="SEX  \
-                   SEX VS DEATH",
-    # Add annotations in the center of the donut pies.
-    annotations=[dict(text='SEX', x=0.19, y=0.5, font_size=10, showarrow=False),
-                 dict(text='SEX VS DEATH', x=0.84, y=0.5, font_size=9, showarrow=False)],
-    autosize=False, width=700, height=500, paper_bgcolor="white")
-
-st.plotly_chart(fig)
